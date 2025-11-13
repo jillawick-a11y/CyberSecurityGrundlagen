49c40a85bdeedeaa1ddf81a7939eeadf01208145
jillawick-a11y <jill.a.wick@gmail.com>
2025-11-13 14:59:29 +0100

    Add files via upload

---
+++ assets/HfPmD9iAiZswMjLq/story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6CQCzGxKOzI":
+        Script1();
+        break;
+      case "5r1OI5rlQ7I":
+        Script2();
+        break;
+      case "6MSuZYHesPE":
+        Script3();
+        break;
+      case "6TUX8VIUg4u":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
