946e6ee935b30e6694f353e63226fdc09e6407ad
jillawick-a11y <jill.a.wick@gmail.com>
2025-11-13 14:58:49 +0100

    Add files via upload

---
+++ assets/BGP8wH-iY8mUHgGg/story_content/user.js
@@ -0,0 +1,64 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  if (/.html/.test(window.location.href)) {
+if (!window.movedCaptions){
+var sheet = document.createElement('style');
+sheet.innerHTML = ".caption{margin-bottom:35px !important;}";
+document.body.appendChild(sheet);
+window.movedCaptions = true;
+}
+}
+}
+
+window.Script2 = function()
+{
+  if (/.html/.test(window.location.href)) {
+if (!window.movedCaptions){
+var sheet = document.createElement('style');
+sheet.innerHTML = ".caption{margin-bottom:35px !important;}";
+document.body.appendChild(sheet);
+window.movedCaptions = true;
+}
+}
+}
+
+window.Script3 = function()
+{
+  if (/.html/.test(window.location.href)) {
+if (!window.movedCaptions){
+var sheet = document.createElement('style');
+sheet.innerHTML = ".caption{margin-bottom:35px !important;}";
+document.body.appendChild(sheet);
+window.movedCaptions = true;
+}
+}
+}
+
+window.Script4 = function()
+{
+  if (/.html/.test(window.location.href)) {
+if (!window.movedCaptions){
+var sheet = document.createElement('style');
+sheet.innerHTML = ".caption{margin-bottom:35px !important;}";
+document.body.appendChild(sheet);
+window.movedCaptions = true;
+}
+}
+}
+
+};
