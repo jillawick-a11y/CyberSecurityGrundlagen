2d865604ce461d3e69543f3cf53f7aa6dbf003b0
jillawick-a11y <jill.a.wick@gmail.com>
2026-05-12 21:04:51 +0200

    Add files via upload

---
+++ assets/LtvQ6KsQ7rRriuXR/story_content/user.js
@@ -0,0 +1,56 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  if (/.html/.test(window.location.href)) {
+if (!window.movedCaptions){
+var sheet = document.createElement('style');
+sheet.innerHTML = ".caption{margin-bottom:35px !important;}";
+document.body.appendChild(sheet);
+window.movedCaptions = true;
+}
+}
+}
+
+window.Script2 = function()
+{
+  var player = GetPlayer();
+var userInput = player.GetVar("Antwort");
+var validAnswer = "Quintacoira Kaffeerösterei";
+
+if (userInput && userInput.toLowerCase().trim() === validAnswer.toLowerCase()) {
+    player.SetVar("AntwortRichtig", true);
+} else {
+    player.SetVar("AntwortRichtig", false);
+}
+}
+
+window.Script3 = function()
+{
+  if (/.html/.test(window.location.href)) {
+if (!window.movedCaptions){
+var sheet = document.createElement('style');
+sheet.innerHTML = ".caption{margin-bottom:35px !important;}";
+document.body.appendChild(sheet);
+window.movedCaptions = true;
+}
+}
+}
+
+};
